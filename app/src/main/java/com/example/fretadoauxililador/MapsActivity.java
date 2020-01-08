e7cf0eb7320b1cabfd5704297288c03ea5874dc8
Cayo Diebe <cayo.diebe@konia.com.br>
2020-01-08 09:18:01 -0200

    Mudanças visuais e começo do desenvolvimento.

--- app/src/main/java/com/example/fretadoauxililador/MapsActivity.java
+++ app/src/main/java/com/example/fretadoauxililador/MapsActivity.java
@@ -9,35 +9,25 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private String[] permissoes = new String[]{
-            Manifest.permission.ACCESS_FINE_LOCATION
-    };
+    private String[] permissoes = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
     private LocationManager locationManager;
     private LocationListener locationListener;
 
@@ -45,11 +35,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
 
         //ValidarPermissões
         Permissoes.validarPermissoes(permissoes, this, 1);
 
-        setContentView(R.layout.activity_main);
+
+        //Setar Variáveis
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -59,6 +52,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng from = new LatLng(-23.4960716, -47.4723136);
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(from);
+        circleOptions.radius(2000);
+        circleOptions.fillColor(Color.argb(128, 255, 153, 0));
+        circleOptions.strokeWidth(10);
+        circleOptions.strokeColor(Color.GREEN);
+        mMap.addCircle(circleOptions);
+
+        mMap.addMarker(new MarkerOptions()
+                .position(from)
+                .title("De")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_house)));
 
         //Recuperar Localização Usuário
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -71,23 +79,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 mMap.clear();
                 LatLng your = new LatLng( latitude,longitude);
-                mMap.addMarker(new MarkerOptions().position(your).title("Sua posição"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(your,15));
-
-
-                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-
-                try {
-                    // List<Address> listaEndereco = geocoder.getFromLocation(latitude,longitude,1);
-                    String address = "Rua Comendador Oeterrer";
-                    List<Address> listaEndereco = geocoder.getFromLocationName(address,1);
-                    if(listaEndereco != null && listaEndereco.size() > 0 ){
-                        Address endereco = listaEndereco.get(0);
-
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                CircleOptions circleOptions = new CircleOptions();
+                circleOptions.center(your);
+                circleOptions.radius(2000);
+                circleOptions.fillColor(Color.argb(128, 65, 85, 90));
+                circleOptions.strokeWidth(1);
+                circleOptions.strokeColor(Color.BLUE);
+                mMap.addCircle(circleOptions);
+                mMap.addMarker(new MarkerOptions().position(your).title("Sua posição")
+                        .icon(
+                                BitmapDescriptorFactory.fromResource(R.drawable.icon_you)));
+                //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(your,14));
 
                 Log.d("Localização","onLocationChanged"+location);
 
@@ -109,6 +111,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         };
+
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER,
@@ -117,49 +120,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     locationListener);
             return;
         }
-
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        // Add a marker in Sydney and move the camera
-        LatLng from = new LatLng(-23.4960716, -47.4723136);
-        CircleOptions circleOptions = new CircleOptions();
-        circleOptions.center(from);
-        circleOptions.radius(2000);
-        circleOptions.fillColor(Color.argb(128, 255, 153, 0));
-        circleOptions.strokeWidth(10);
-        circleOptions.strokeColor(Color.GREEN);
-        mMap.addCircle(circleOptions);
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions()
-                        .position(latLng)
-                        .snippet("Descrição")
-                        .title("De")
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_background)));
-            }
-        });
-
-        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions()
-                        .position(latLng)
-                        .snippet("Descrição")
-                        .title("De")
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_background)));
-            }
-        });
-
-        mMap.addMarker(new MarkerOptions()
-                .position(from)
-                .title("De")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_background)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(from, 10));
     }
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
